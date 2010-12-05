3285055c356551e2fcf083651af5342b64ecb260
Panbox <panbox@atlas.panjunction.com>
2010-12-04 18:40:47 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fahstats::Application.config.secret_token = '0d70c1dbddd30b44fb219793bdb95f29ba0c78bcdb86c6fa68f8815990c2f5e1547f5483d1543efc80288b3c34c244080eb6bf8ec26e74f7dbb37465c3658e38'
